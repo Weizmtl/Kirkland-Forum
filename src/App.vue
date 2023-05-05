b59215eb34ddd14cbb4473b853dc1ae7eccdc88c
Weizmtl <eric.zmtl@gmail.com>
2023-05-04 22:06:41 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <el-button type="primary">button</el-button>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
+
+
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import VueCookies from 'vue-cookies'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import './assets/base.scss'
+import '@/assets/icon/iconfont.css'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(ElementPlus);
+app.config.globalProperties.VueCookies = VueCookies;
+
+
+app.mount('#app')
